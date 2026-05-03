8a78e175912fd7c4eca20bc19e9b71e46a1e1ecf
MartinAjii <martin.aji789@gmail.com>
2026-05-03 23:53:06 +0700

    commit pull

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -5,7 +5,7 @@ class ApiConfig {
     if (kIsWeb) {
       return "http://localhost:5000";
     } else {
-      return "http://192.168.0.101:5000";
+      return "http://10.232.158.204:5000";
     }
   }
 }
